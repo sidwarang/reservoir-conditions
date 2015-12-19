f22e3822c921f84ea71b9cb5c42d6d76341db7d1
Siddharth Warang <siddharth.warang@sjsu.edu>
2015-12-19 11:11:39 -0800

    cleaned and refactored

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ app.use(bodyParser.json());
 var fs = require('fs');
 var json;
 fs.readFile('locations.json', 'utf8', function(err, data){
-if(err) {
-	throw err;
-}
-else{
-	json = data;
-}
+    if(err) {
+        throw err;
+    }
+    else{
+        json = data;
+    }
 });
 var port = 8080;
 
 app.get('/', function(request, response){
-response.setHeader('access-control-allow-origin', '*');
-response.send(json);
+    response.setHeader('access-control-allow-origin', '*');
+    response.send(json);
 }) 
 
 app.listen(port, function() {
-console.log('Server started... Listening at port 8080')
+    console.log('Server started... Listening at port 8080')
 });
